16d7c14a5863578ca99db8993f37c927505b1cd5
Chandu-bs <chandu@bluesapience.com>
2022-04-07 14:28:59 +0530

    widget test errors solved

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 // ignore_for_file: deprecated_member_use
 
 import 'dart:ffi';
-
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -56,17 +56,26 @@ testWidgets(
   "Checking tap function",
   (WidgetTester tester) async {
 
-    
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   await tester.pumpWidget(MaterialApp(home: Scaffold(body: DicePage())));
   await tester.pump();
   
-  expect(find.image(const AssetImage('images/dice1.png')), findsNWidgets(2));
+  
+  Widget myButton = FlatButton(onPressed: (() { changeDiceFace(); }), child: Image.asset('images/dice1.png'));
 
-  tester.widget(find.byKey(const ValueKey("diceNo")));
-  await tester.tap(find.byKey(const ValueKey("diceNo")));
+  await tester.tap(find.byWidget(myButton));
+  await tester.pump();
 
+  expect(find.image(AssetImage("images/dice$leftDiceNumber.png")), findsOneWidget);
+  expect(find.image(AssetImage("images/dice$rightDiceNumber.png")), findsOneWidget);
 
-  expect(find.image(const AssetImage('images/')), findsOneWidget);
 
   },
 );
